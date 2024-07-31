65e918b17a05da28df56deb37402160126bdc923
rishika-on-git <aggarwalrishika135@gmail.com>
2024-07-31 14:16:02 +0530

    metadata change

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ import { ThemeProvider } from "@/components/layout/theme-provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Shadcn - Landing template",
-  description: "Landing template from Shadcn",
+  title: "CICE- JIIT NOIDA",
+  description: "Creativity and Innovation Cell in Electronics",
 };
 
 export default function RootLayout({
