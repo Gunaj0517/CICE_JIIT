3b7b9a6a7d384a3ae1cfabc631ef2a8a1ad15131
rishika-on-git <aggarwalrishika135@gmail.com>
2024-08-05 22:52:04 +0530

    added text and imgs

--- components/layout/sections/footer.tsx
+++ components/layout/sections/footer.tsx
@@ -9,8 +9,11 @@ export const FooterSection = () => {
         <div className="grid grid-cols-2 md:grid-cols-2 xl:grid-cols-4 gap-x-12 gap-y-8">
           <div className="col-span-full xl:col-span-2">
             <Link href="#" className="flex font-bold items-center">
-              <ChevronsDownIcon className="w-9 h-9 mr-2 bg-gradient-to-tr from-primary via-primary/70 to-primary rounded-lg border border-secondary" />
-
+              <div className="w-9 h-13 mr-2 rounded-lg">
+              
+              
+              <img src="/logo.png"/>
+              </div>
               <h3 className="text-2xl">CICE</h3>
             </Link>
           </div>
@@ -36,9 +39,6 @@ export const FooterSection = () => {
             </div>
           </div>
 
-       
-          
-
           <div className="flex flex-col gap-2">
             <h3 className="font-bold text-lg">Socials</h3>
             <div>
@@ -58,7 +58,6 @@ export const FooterSection = () => {
                 Instagram
               </Link>
             </div>
-           
           </div>
         </div>
 
